e951a0f70b47c825a560a90b111fb4d0f0073714
Anukriti Bagchi <anukritibagchi98@gmail.com>
2026-02-09 08:58:36 +0530

    added logic for prime number

--- basics2.js
+++ basics2.js
@@ -27,15 +27,15 @@ console.log(i)
 
 // 2 and 5
 // from 1 to 10, give me the common multiples of 2 and 5 
-console.log('*******************')
+/*console.log('*******************')
 let n =0
-for (let k=1;k<=100;k++)
+for (let k=1; k<=200; k++)
 {
-    if (k%2 == 0 && k%5 == 0)
+    if (k%3 == 0 && k%7 == 0)
     {
      n++
      console.log(k)
-     if (n==3)
+     if (n==5)
      break
         
     }
@@ -47,7 +47,31 @@ while(required)
 {
     console.log(required)
     required = false
-}
+}*/
 
- 
+
+
+ let n = 0
+
+ for (let j = 2; j<=100; j++)
+ {
+    let isPrime = true;
+
+    for (let k=2; k < j; k++) //check all smaller numbers
+    {
+        if (j%k == 0) //if j is divisible by any smaller number, it's not prime
+        {
+            isPrime = false
+            break
+        }
+    }
+
+    if (isPrime) {
+        console.log(j)
+        n++;        
+        if (n === 3) 
+        break;
+    }
+ }
+
 
