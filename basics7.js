dd1d17272a68a2be4e0639b417774f1f6068f27e
Anukriti Bagchi <anukritibagchi98@gmail.com>
2026-02-24 17:47:27 +0530

    Add Person getter demo and log age/location

---
+++ basics7.js
@@ -0,0 +1,12 @@
+class Person
+{
+   age = 25
+   get location()
+   {
+    return "Canada \nSeattle \nDetroit \nManhattan"
+   }
+}
+
+let person = new Person()
+console.log(person.age)
+console.log(person.location)
